ec4f3e99e2afb91d7282cc213cffeac75c9b0f2c
zhaomingxuan <134053539+zhaomingxuan96@users.noreply.github.com>
2024-09-04 14:38:53 +0800

    路由更改为历史模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,4 @@
-import { createRouter as _createRouter, createWebHashHistory } from 'vue-router'
-
-// const pages = import.meta.glob('../src/pages/**/*.vue')
-// const routes = Object.keys(pages).map((path)=>{
-//   let name = path.match(/\/pages(.*)\.vue$/)[1].toLowerCase();
-//   if(name.substring(name.length - 5) == 'index'){
-//     name = name.slice(0, -5);//去掉最后的index
-//   }
-//   return {
-//     path: name === '/home' ? '/': name,
-//     component: pages[path]
-//   }
-// })
+import { createRouter as _createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
@@ -118,7 +106,7 @@ const routes = [
 
 export function createRouter() {
   return _createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes
   })
 }
